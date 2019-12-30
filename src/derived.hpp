37b1c08857222a7e5e4c20a7285bd8104b429788
Alexander Larin <scalar438@gmail.com>
2019-12-30 16:51:16 +0300

    Format file

--- src/derived.hpp
+++ src/derived.hpp
@@ -4,11 +4,11 @@
 class Derived1 : public Base
 {
 public:
-    const std::string &type() const override;
+	const std::string &type() const override;
 };
 
 class Derived2 : public Base
 {
 public:
-    const std::string &type() const override;
+	const std::string &type() const override;
 };
