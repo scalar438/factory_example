4deed4227fd5848e07bbcdd4e2c113a819d870b1
Alexander Larin <scalar438@gmail.com>
2024-12-29 12:58:40 +0500

    Delete the 'factory_simple' project

--- factory_simple/constants.cpp
+++
@@ -1,9 +0,0 @@
-#include "constants.hpp"
-
-class Derived1;
-template <> const std::string Types<Derived1>::type = "Derived1";
-
-class Derived2;
-template <> const std::string Types<Derived2>::type = "Derived2";
-
-// If we forget to define type for some classes, we will get linker error
