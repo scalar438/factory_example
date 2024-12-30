faadbe5a85e06d63be0cce5ddb6292ad21392476
Alexander Larin <scalar438@gmail.com>
2024-12-30 09:10:01 +0500

    Compiled (and works) in windows

--- factory_dynamic/main_program/plugin_win.cpp
+++ factory_dynamic/main_program/plugin_win.cpp
@@ -7,10 +7,11 @@ struct Plugin::Pimpl
 	HMODULE handle;
 };
 
-Plugin::Plugin(std::filesystem::path dll_name) : pimpl(std::make_unique<Pimpl>())
+Plugin::Plugin(const std::string &lib_name) : pimpl(std::make_unique<Pimpl>())
 {
-	auto hlib = LoadLibrary(dll_name.c_str());
-	if (hlib == NULL) throw PluginLoadException(dll_name.generic_string());
+	auto dll_name = lib_name + ".dll";
+	auto hlib     = LoadLibraryA(dll_name.c_str());
+	if (hlib == NULL) throw PluginLoadException(dll_name);
 	pimpl->handle = hlib;
 }
 
